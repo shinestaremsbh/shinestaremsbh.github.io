47b6435ba6b5e67134bd6321c3e242000b815101
star <pidsmep@gmail.com>
2026-03-09 19:50:04 +0300

    Remove smooth scroll navigation from script.js
    Removed smooth scroll navigation functionality.

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-
-// Smooth scroll navigation
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    const id = this.getAttribute('href');
-    document.querySelector(id).scrollIntoView({
-      behavior:'smooth'
-    });
-  });
-});
+console.log("Website loaded successfully");
